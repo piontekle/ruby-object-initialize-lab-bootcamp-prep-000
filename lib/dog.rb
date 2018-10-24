03d4495f38f0a6ff72e82ff95e392b20431bc4e6
Lauren Piontek <piontekle@gmail.com>
2018-10-24 16:34:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,16 @@
 class Dog 
-  def initialize(breed = "Mutt")
+  def initialize(name, breed = "Mutt")
+    @name = name
     @breed = breed
   end
+  
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
   def breed=(breed)
     @breed = breed
   end
